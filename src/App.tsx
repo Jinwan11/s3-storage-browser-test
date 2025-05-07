d90ec29931ec02d40cb214661bb0085fadb634e7
jlee1 <jinwan.i@nsw.co.jp>
2025-05-07 15:38:01 +0900

    add s3 backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
+// import { useAuthenticator } from '@aws-amplify/ui-react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { StorageBrowser } from '../components/StorageBrowser';
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
   // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
